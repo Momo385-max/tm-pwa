e50531b0df624972ad53c5eb8a9e521543d1b00d
Momo385-max <moritz.creuznacher@gmail.com>
2025-08-05 23:23:01 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='tm-pwa-ios-forced-v2';
+const CACHE_NAME='tm-pwa-ios-forced-v9';
 const CORE_ASSETS=['/tm-pwa/','/tm-pwa/index.html','/tm-pwa/app.js','/tm-pwa/manifest.webmanifest','/tm-pwa/assets/icon-180.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(CORE_ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
