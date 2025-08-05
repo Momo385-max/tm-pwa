57bb3ca9fd1674610641e5db428a3f76a7785b0e
Momo385-max <moritz.creuznacher@gmail.com>
2025-08-05 23:18:38 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // SW with absolute scope and network-first for /tm-pwa/model/ to avoid stale caches
-const CACHE_NAME = 'tm-pwa-ios-forced-v7';
+const CACHE_NAME = 'tm-pwa-ios-forced-v8';
 const CORE_ASSETS = [
   '/tm-pwa/',
   '/tm-pwa/index.html',
