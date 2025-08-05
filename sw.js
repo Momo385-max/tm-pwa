545491d9ca0e3f50c8918c8e8b8b42457cd14ddb
Momo385-max <moritz.creuznacher@gmail.com>
2025-08-05 22:16:30 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // SW with bumped cache & offline; lazy libs will be cached after first load
-const CACHE_NAME = 'tm-pwa-ios-lazy-v3';
+const CACHE_NAME = 'tm-pwa-ios-lazy-v4';
 const CORE_ASSETS = [
   './',
   './index.html',
